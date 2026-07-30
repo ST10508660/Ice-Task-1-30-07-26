13d0e9c77cf266256dfd26e3b1cf90dc434a86fe
ST10508660 <ST10508660@myemeris.edu.za>
2026-07-30 21:05:48 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/ice_task_1/Ice_Task_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ice_task_1;
+
+/**
+ *
+ * @author User
+ */
+public class Ice_Task_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
