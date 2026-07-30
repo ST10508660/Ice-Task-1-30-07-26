56ee1bfe4e380dd349fe6d04104555bf8fa7481c
ST10508660 <ST10508660@myemeris.edu.za>
2026-07-30 21:22:36 +0200

    refined bubble sort

--- src/main/java/com/mycompany/ice_task_1/Ice_Task_1.java
+++ src/main/java/com/mycompany/ice_task_1/Ice_Task_1.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.ice_task_1;
-
+import java.util.Scanner;
 /**
  *
  * @author User
@@ -11,17 +11,19 @@ package com.mycompany.ice_task_1;
 public class Ice_Task_1 {
 
     public static void main(String[] args) {
-       
+       Scanner rs= new Scanner(System.in);
+        System.out.println("Enter the number of students");
+        int noStudents=rs.nextInt();
+        int[][] storage;
     }
-    public static void bubblesort(double[] valuesToSort)
-    {
-         // Bubble sort (descending order)
+    public static void bubbleSortDescending(int[][] array) {
+        int n = array.length;
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n-1 - i; j++) {          
-                if (valuesToSort[j] < valuesToSort[j + 1]) {     
-                    double temp = valuesToSort[j];
-                    valuesToSort[j] = valuesToSort[j + 1];
-                    valuesToSort[j + 1] = temp;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j][4] < array[j + 1][4]) {
+                    int[] temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
         }
