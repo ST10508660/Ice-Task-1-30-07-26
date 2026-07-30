9940488d4d38d586e9c45c29e3d7db7eef7fd06c
ST10508660 <ST10508660@myemeris.edu.za>
2026-07-30 21:10:44 +0200

    added base bubble sort function

--- src/main/java/com/mycompany/ice_task_1/Ice_Task_1.java
+++ src/main/java/com/mycompany/ice_task_1/Ice_Task_1.java
@@ -11,6 +11,19 @@ package com.mycompany.ice_task_1;
 public class Ice_Task_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
+    }
+    public static void bubblesort(double[] valuesToSort)
+    {
+         // Bubble sort (descending order)
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n-1 - i; j++) {          
+                if (valuesToSort[j] < valuesToSort[j + 1]) {     
+                    double temp = valuesToSort[j];
+                    valuesToSort[j] = valuesToSort[j + 1];
+                    valuesToSort[j + 1] = temp;
+                }
+            }
+        }
     }
 }
